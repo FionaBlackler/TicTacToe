9d723bcf1e4734025459c92e23e225d3285235d9
02mato1bif <02mato1bif@hft-stuttgart.de>
2022-01-08 19:53:56 +0100

    add Working Socket.io example to build on

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/pages/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('disconnect', function(){
+
+  socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 
-  socket.on('message', function(msg){
+  socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
-    io.emit('message', msg);
+    io.emit('chat message', msg);
   });
+
 });
 
-http.listen(port, function(){
+server.listen(3000, () => {
   console.log('listening on *:3000');
 });
