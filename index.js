4b21e36858237fc9b806c1948bdfd30aaa7c2cff
FionaBlackler <02blfi1bif@hft-stuttgart.de>
2022-01-17 16:38:11 +0100

    install socket.io

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
 const express = require('express');
-const path = require("path");
-
-// App initialisierung
-const port = process.env.PORT || 3000;
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = process.env.PORT || 3000;
 
-// App Konfiguration
-app.use('/public', express.static(path.join(__dirname, 'public')))
-app.set('view engine', 'ejs');
-
-// App request
-// GET Requests vgl. PHP $_GET, aber auch eben auch im Browser localhost:3000
-app.get('/', function (req, res) {
-  res.render('pages/index');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/pages/index.html');
 });
 
-app.get('/about', function(req, res) {
-  res.render('pages/about');
+app.use(express.static(__dirname + '/public'));
+
+//User Connects
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  //User Disconnects
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  //Server get data and reacts
+
+  socket.on('player won', (data) => {
+    console.log('player: ' + data + " won");
+    io.emit('player won', data);
+  });
+
+  socket.on('player tie', (data) => {
+    console.log("its a tie");
+    io.emit('player tie', data);
+  });
+
+  socket.on('player move', (data) => {
+    console.log('message: ' + data);
+    io.emit('player move', data);
+  });
+
+  socket.on('reset board', (data) => {
+    console.log('message: ' + data);
+    io.emit('reset board', data);
+  });
+
 });
 
-// App start
-// POST Requests vgl. PHP $_POST
-app.listen(port, () => {
- console.log('Server ist running on port: '+ port);
+server.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
